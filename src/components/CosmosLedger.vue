1b7c3d9804bf0b4a86b9fda1e9d5478e0f33258d
Juan Leni <lenijuan@gmail.com>
2019-06-02 14:01:32 +0200

    init

---
+++ src/components/CosmosLedger.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cosmosLedger">
+    <h3>Detect device</h3>
+    <h3>Retrieve balance + delegations</h3>
+    <h3>Delegate</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CosmosLedger',
+  props: {
+    restUrl : String,
+    someProp: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
